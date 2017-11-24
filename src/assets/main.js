b7de207b36bbb8780a052ecf6b5f78e4571fe25e
alekobri <sunburst@mailbox.org>
2017-11-24 17:23:31 +0100

    First try in building

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,39 @@
+
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/Naypersand.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      showBadges(response.courses.completed);
+    }
+  });
+
+  function showBadges(courses) {
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+      var $div = $('<div />', {
+        'class': 'course'
+      });
+
+      $badges.append($div);
+
+      $div.append($('<h3 />', {
+        text: course.title
+      }));
+
+      $div.append($('<img />', {
+        src: course.badge
+      }));
+
+      $div.append($('<a />', {
+        href: course.url,
+        target: '_blank',
+        'class': 'btn btn-primary',
+        text: 'See Course'
+      }));
+    });
+  }
 
 });
